8b8d6b817fe2142c0f2bd81dffc04abde1cfc568
Ian Darwin <ian@darwinsys.com>
2017-10-31 18:38:26 -0400

    XML 0, JSON 1

--- src/main/java/com/darwinsys/clientware/UrlJaxbClient.java
+++ src/main/java/com/darwinsys/clientware/UrlJaxbClient.java
@@ -1,18 +1,12 @@
 package com.darwinsys.clientware;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.ws.rs.core.MediaType;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.Document;
 
 /**
  * A JAX-RS client NOT using the JAX-RS Client API
@@ -25,15 +19,13 @@ public class UrlJaxbClient {
 	public static void main(String[] args) throws Exception {
 		URL url = new URL(BASE_URL + "/4");
 		final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		connection.addRequestProperty("Accept", MediaType.APPLICATION_XML);
+		connection.addRequestProperty("Accept", MediaType.APPLICATION_JSON);
 		InputStream is = connection.getInputStream();
 		// Read "is" to get the response from a GET
-
-		// Read into a DOM Tree and print to stdout
-		Document tree = // DOM API
-				DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(is);
-		Transformer tx = TransformerFactory.newInstance().newTransformer();
-		tx.setOutputProperty(OutputKeys.INDENT, "yes");
-		tx.transform(new DOMSource(tree), new StreamResult(System.out));
+		BufferedReader bi = new BufferedReader(new InputStreamReader(is));
+		String line = null;
+		while ((line = bi.readLine()) != null) {
+			System.out.println(line);
+		}
 	}
 }
