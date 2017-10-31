9c3b1a55f9c9eb1292c7c21148f6f42eae450a64
Ian Darwin (at course) <ian@darwinsys.com>
2017-10-31 16:37:11 -0400

    Simple SslClient now works.

--- src/main/java/com/darwinsys/clientware/SimpleSslClient.java
+++ src/main/java/com/darwinsys/clientware/SimpleSslClient.java
@@ -12,10 +12,10 @@ public class SimpleSslClient {
 	public static void main(String[] args) {
 
 		String baseUrl = "https://oracle.com/";
-
+		System.out.println("Opening " + baseUrl + " with NO SSL Config");
 		Client client = ClientBuilder.newClient();
 
-		Response resp = client.target(baseUrl).path("/index.web").request().get();
+		Response resp = client.target(baseUrl).path("/").request().get();
 
 		System.out.println("Response was: " + resp.getStatus());
 	}
