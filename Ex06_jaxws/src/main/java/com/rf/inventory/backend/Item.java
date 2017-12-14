aa743948cde7f3d234a0cdd525d3f42ad7df398f
Ian Darwin <ian@darwinsys.com>
2017-12-14 18:45:32 -0500

    Clean up JAXB

--- Ex06_jaxws/src/main/java/com/rf/inventory/backend/Item.java
+++ Ex06_jaxws/src/main/java/com/rf/inventory/backend/Item.java
@@ -3,22 +3,20 @@
  */
 package com.rf.inventory.backend;
 
-import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * Data type corresponding to XML exchanged with the server. The XML looks like this:
  * <item productId="3212" quantity="4" />
  * 
  * @author v.lakshmanan
- *
  */
 
 // TODO: Add annotation to indicate that "item" should be used as the root element of 
 //       the exchanged XML
 // HINT: See slide 6-19
-
+@XmlRootElement
 public class Item {
     private int productId;
     private int quantity;
@@ -33,7 +31,7 @@ public class Item {
 
     // TODO: Add annotation to indicate that productId is an attribute 
     //       of the item, not a child element
-    
+    @XmlAttribute
     public int getProductId() {
         return productId;
     }
@@ -44,7 +42,7 @@ public class Item {
     
     // TODO: Add annotation to indicate that quantity is an attribute of item, 
     // not a child element
-    
+    @XmlAttribute
     public int getQuantity() {
         return quantity;
     }
