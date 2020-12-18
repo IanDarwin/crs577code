3e761b39ecbec847f35293054007953f2c32012a
Ian Darwin <ian@darwinsys.com>
2020-12-18 11:55:35 +0530

    Comment the base URL

--- crs577add-demos/src/main/java/com/darwinsys/clientware/Constants.java
+++ crs577add-demos/src/main/java/com/darwinsys/clientware/Constants.java
@@ -1,6 +1,9 @@
 package com.darwinsys.clientware;
 
 public class Constants {
+	/** The current Android Cookbook site makes recipes
+	 * viewable at this URL with a recipeId as a URL Param
+	 */
 	public static final String ACB_BASE_URL = 
 		"https://androidcookbook.com/rest/recipe";
 }
