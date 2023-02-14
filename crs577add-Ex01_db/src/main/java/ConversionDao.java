df331ed7945b4052bd140d1999dab2b711362550
Ian Darwin <ian@darwinsys.com>
2023-02-14 16:40:47 -0500

    Add faked RealConversionDao. Document ConversionDao.

--- crs577add-Ex01_db/src/main/java/ConversionDao.java
+++ crs577add-Ex01_db/src/main/java/ConversionDao.java
@@ -1,5 +1,9 @@
 import java.math.BigDecimal;
 
+/**
+ * Meant for use only internally inside the FX Service.
+ * Simulates what a real FX service would have to do.
+ */
 public interface ConversionDao {
 
 	BigDecimal getRate(Currency from, Currency to);
---
+++ crs577add-Ex01_db/src/main/java/RealConversionDao.java
@@ -0,0 +1,8 @@
+import java.math.BigDecimal;
+
+class RealConversionDao implements ConversionDao {
+
+	public bigDecimal convert(Currency From, Currency to) {
+		throw new IllegalStateExcpetion("Real db-based converter not written yet");
+	}
+}
